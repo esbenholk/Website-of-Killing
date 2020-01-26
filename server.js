671e314cc288df794d781ef039760e92aadb058a
Esben Holk <esbenholknielsen@gmail.com>
2020-01-26 13:46:06 +0100

    changed port to 8000

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const compression = require("compression");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.use(compression());
 
 app.use(express.static("./public"));
